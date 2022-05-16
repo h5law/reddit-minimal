f7c3156bd026cc35da662fb62b3499cafad8a608
Harry Law <53987565+h5law@users.noreply.github.com>
2022-05-16 14:54:43 +0100

    Add spaces to import breakdown for consistent style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './features/header/Header.js';
 import Home from './features/home/Home.js';
